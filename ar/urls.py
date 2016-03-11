f937d8bd875daa05568ed8cbd7fc05723bb9835c
Jesse Hodge <jessehodge1987@gmail.com>
2016-03-11 14:40:22 -0600

    Updating Urls for ar.views to be imported

--- ar/urls.py
+++ ar/urls.py
@@ -1,16 +1,17 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from ar.views import index, commercial, residential, hometheatre, ssf, contact, thankyou
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'ar.views.index'),
-    url(r'^commercial', 'ar.views.commercial'),
-    url(r'^residential', 'ar.views.residential'),
-    url(r'^hometheatre', 'ar.views.hometheatre'),
-    url(r'^ssf', 'ar.views.ssf'),
-    url(r'^contact', 'ar.views.contact'),
-    url(r'^thankyou', 'ar.views.thankyou'),
+    url(r'^$', index),
+    url(r'^commercial', commercial),
+    url(r'^residential', residential),
+    url(r'^hometheatre', hometheatre),
+    url(r'^ssf', ssf),
+    url(r'^contact', contact),
+    url(r'^thankyou', thankyou),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
