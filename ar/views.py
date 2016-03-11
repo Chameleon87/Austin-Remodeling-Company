762966b2c482dc3b93291012d54d84a1c9698269
Jesse Hodge <jessehodge1987@gmail.com>
2016-03-11 14:47:16 -0600

    Getting rid of unneeded imports

--- ar/views.py
+++ ar/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.template import RequestContext, Context
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django import forms
